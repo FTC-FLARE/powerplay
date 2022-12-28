9a630ab742dbc83e83f2fba201250d48b8e23fcf
Ellie-12833 <elleman4242@gmail.com>
2022-12-27 22:55:10 -0500

    add cone saver code(not tested)

--- src/main/java/org/firstinspires/ftc/teamcode/opmodes2022powerplay/MM_Collector.java
+++ src/main/java/org/firstinspires/ftc/teamcode/opmodes2022powerplay/MM_Collector.java
@@ -5,16 +5,21 @@ import com.qualcomm.robotcore.hardware.Servo;
 public class MM_Collector {
     private final MM_OpMode opMode;
     private Servo grabber = null;
+    private Servo coneSaver = null;
 
     public static final double CLOSED = 0.07; //0.185 - 0.09
     public static final double OPEN = 1.0;
 
     private double position = OPEN;
 
+    private double coneSaverPosition = 1;
+
     public MM_Collector(MM_OpMode opMode) {
         this.opMode = opMode;
         grabber = opMode.hardwareMap.get(Servo.class, "Grabber");
+        coneSaver = opMode.hardwareMap.get(Servo.class,"Conesaver");
         changePosition(OPEN);
+        coneSaver.setPosition(0);
     }
 
     public void runCollector() {
@@ -27,12 +32,21 @@ public class MM_Collector {
         }
         opMode.telemetry.addData("Collector Position", grabber.getPosition());
     }
-
+    public void runConeSaver() {
+        if (opMode.leftBumperPressed(opMode.GAMEPAD2) && !opMode.robot.slide.tooLowToPivot()) {
+            if (coneSaverPosition == 1) {
+                coneSaver.setPosition(0);
+                coneSaverPosition = 0;
+            } else {
+                coneSaver.setPosition(1);
+                coneSaverPosition = 1;
+            }
+        }
+    }
     public void changePosition(double position){
         grabber.setPosition(position);
         this.position = position;
     }
-
     public double getPosition() {
         return position;
     }
